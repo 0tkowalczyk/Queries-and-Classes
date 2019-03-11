0f36ac5d080584561d85cf8e4aac341f92a3a6bf
0tkowalczyk <42749742+0tkowalczyk@users.noreply.github.com>
2019-03-11 16:47:06 -0500

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,48 @@
+function green25(){
+    const doc = document;
+    let body = doc.querySelector("body");
+    body.classList.add("green25")
+}
+function green50(){
+    const doc = document;
+    let body = doc.querySelector("body");
+    body.classList.add("green50")
+}
+
+
+function green75(){
+    const doc = document;
+    let body = doc.querySelector("body");
+    body.classList.add("green75")
+
+}
+
+
+function green100(){
+    const doc = document;
+    let body = doc.querySelector("body");
+    body.classList.add("green100")
+
+}
+
+
+function smallToggle(){
+    const doc = document;
+    let divs = doc.querySelectorAll("sections")
+    divs.classList.add("quarter_box")
+
+}
+
+
+function mediumToggle(){
+    const doc = document;
+    let divs = doc.querySelectorAll("divs")
+    divs.classList.add("half_box")
+}
+
+
+function largeToggle(){
+    const doc = document;
+    let divs = doc.querySelectorAll("divs")
+    divs.classList.add("full_box")
+}
